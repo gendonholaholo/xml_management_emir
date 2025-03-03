417f93f0d6ddac745fb4af8b19a96d2fef2a9981
Mas Gendon <67081785+gendonholaholo@users.noreply.github.com>
2025-03-03 20:28:25 +0700

    lazygit

--- apps/process_xml.py
+++ apps/process_xml.py
@@ -43,3 +43,4 @@ for filename in os.listdir(source_dir):
             logging.error(f"Error saat memproses {filename}: {e}")
 
 print("\nProses selesai! Semua file telah disimpan di folder 'modified'.")
+print("\nProses selesai! Semua file telah disimpan di folder 'modified'.")
